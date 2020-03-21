459289117cd4d2a57ba3a094e0ad7d0860678bed
JonathanvdGeer <55492477+JonathanvdGeer@users.noreply.github.com>
2020-03-21 22:35:15 +0100

    Gallery with Modal Fix

--- js/script.js
+++ js/script.js
@@ -76,13 +76,33 @@ function Melkweg() {
     }
 }
 
+function Travelguide() {
+    var modal = document.getElementById("Travelguide");
 
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[1];
+
+    // When the user clicks the button, open the modal 
+    modal.style.display = "block";
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
 
 function Aquabuddy() {
     var modal = document.getElementById("Aqua-Buddy");
 
     // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[1];
+    var span = document.getElementsByClassName("close")[2];
 
     // When the user clicks the button, open the modal 
     modal.style.display = "block";
@@ -105,7 +125,7 @@ function NPD() {
     var modal = document.getElementById("NPD");
 
     // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[2];
+    var span = document.getElementsByClassName("close")[3];
 
     // When the user clicks the button, open the modal 
     modal.style.display = "block";
@@ -123,11 +143,81 @@ function NPD() {
     }
 }
 
+
+function Dream() {
+    var modal = document.getElementById("Dream");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[4];
+
+    // When the user clicks the button, open the modal 
+    modal.style.display = "block";
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+
+
+function Verhalen() {
+    var modal = document.getElementById("Verhalen");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[5];
+
+    // When the user clicks the button, open the modal 
+    modal.style.display = "block";
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+
+
 function Pizza() {
     var modal = document.getElementById("Singlepage");
 
     // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[3];
+    var span = document.getElementsByClassName("close")[6];
+
+    // When the user clicks the button, open the modal 
+    modal.style.display = "block";
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+
+
+function Ninjawarrior() {
+    var modal = document.getElementById("Ninjawarrior");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[7];
 
     // When the user clicks the button, open the modal 
     modal.style.display = "block";
@@ -150,7 +240,29 @@ function Hvana() {
     var modal = document.getElementById("Hvana");
 
     // Get the <span> element that closes the modal
-    var span = document.getElementsByClassName("close")[4];
+    var span = document.getElementsByClassName("close")[8];
+
+    // When the user clicks the button, open the modal 
+    modal.style.display = "block";
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+
+function Visualport() {
+    var modal = document.getElementById("Visualport");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[9];
 
     // When the user clicks the button, open the modal 
     modal.style.display = "block";
@@ -170,9 +282,13 @@ function Hvana() {
 
 
 
-
 document.getElementById('Melkweg-poster').addEventListener('click', Melkweg)
+document.getElementById('Travelguide-poster').addEventListener('click', Travelguide)
 document.getElementById('Aquabuddy-poster').addEventListener('click', Aquabuddy)
-document.getElementById('Vormgeving-poster').addEventListener('click', Pizza)
-document.getElementById('Hvana-poster').addEventListener('click', Hvana)
 document.getElementById('NPD-poster').addEventListener('click', NPD)
+document.getElementById('Dream-poster').addEventListener('click', Dream)
+document.getElementById('Verhalen-poster').addEventListener('click', Verhalen)
+document.getElementById('Vormgeving-poster').addEventListener('click', Pizza)
+document.getElementById('Ninjawarrior-poster').addEventListener('click', Ninjawarrior)
+document.getElementById('Hvana-poster').addEventListener('click', Hvana)
+document.getElementById('Visualport-poster').addEventListener('click', Visualport)
